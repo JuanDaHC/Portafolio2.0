887761bee2ee0c4515b333af2e03d7d089da2310
alexander muñoz <alexander12345my@gmail.com>
2023-08-19 00:38:09 -0500

    cards 80%

--- src/views/projects/Projects.tsx
+++ src/views/projects/Projects.tsx
@@ -1,29 +1,37 @@
-import styles from './projects.module.css'
-import arrow from '../../assets/arrow.svg'
-import data from '../../data'
+import styles from "./projects.module.css";
+import arrow from "../../assets/arrow.svg";
+import data from "../../data";
 
 const Projects = () => {
-    return(
-        <section className={styles.projects}>
-            <span className='flex  items-center gap-2'>
-                <h2 className='text-2xl text-primary-color font-bold'>Projects</h2>
-                <img className='w-7' src={arrow} alt="flecha" />
-            </span>
+  return (
+    <section className={styles.projects}>
+      <span className="flex  items-center gap-2">
+        <h2 className="text-2xl text-primary-color font-bold">Projects</h2>
+        <img className="w-7" src={arrow} alt="flecha" />
+      </span>
 
-            <article>
-                {
-                    data.map(card => {
-                        return(
-                            <div key={card.id}>
-                                <img src={card.image} alt="" />
-                            </div>
-                        )
-                    })
-                }
+      <article className={styles.cards}>
+        {data.map((card) => {
+          return (
+            <article className={styles.card} key={card.id}>
+              <img className={styles.img} src={card.image[0]} />
+              <h3>{card.name}</h3>
+              <p>{card.descrip}</p>
+              <div key={card.id}>
+                {card.tecno.map((tec) => {
+                  return <img src={tec} alt="" />;
+                })}
+              </div>
+              <div>
+                <a href={card.url}>URL</a>
+                <a href={card.urlGithub}>GITHUB</a>
+              </div>
             </article>
-            
-        </section>
-    )
-}
+          );
+        })}
+      </article>
+    </section>
+  );
+};
 
 export default Projects;
