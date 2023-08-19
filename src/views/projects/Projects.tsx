7ea6cbf2bd636cb9bb8f0cd38b5d21a6c3f23885
alexander muñoz <alexander12345my@gmail.com>
2023-08-19 16:07:55 -0500

    projects 90%

--- src/views/projects/Projects.tsx
+++ src/views/projects/Projects.tsx
@@ -1,9 +1,11 @@
 import styles from "./projects.module.css";
 import arrow from "../../assets/arrow.svg";
 import data from "../../data";
+import github from '../../assets/github.svg'
+import url from '../../assets/url.svg'
+
 
 const Projects = () => {
-  
   return (
     <section className={styles.projects}>
       <span className="flex  items-center gap-2">
@@ -11,28 +13,20 @@ const Projects = () => {
         <img className="w-7" src={arrow} alt="flecha" />
       </span>
 
-      <article className={`${styles.cards} ${styles.containerimg}`}>
+      <article className={`${styles.cards}  `}>
         {data.map((card) => {
           return (
-            <div className={`${styles.card} ${styles.boximg}`} key={card.id}>
-              <a href="#">
-                <figure>
-                  <img src={card.image[0]} alt="" />
-                </figure>
-              </a>
-              
-              {/* <h3>{card.name}</h3>
-              <p>{card.descrip}</p>
-              <div key={index} className="flex">
-                {card.tecno.map((tec) => {
-                  return <img src={tec} alt="" />;
-                })}
+            <div className={`${styles.card}`} key={card.id}>
+              <div className={styles.removewhenuse}>
+                <img src={card.image[0]} alt="" />
               </div>
-              <div>
-                <a href={card.url}>URL</a>
-                <a href={card.urlGithub}>GITHUB</a> 
-              </div> */}
-            
+              <span className={styles.details}>
+                <label>{card.name}</label>
+                <div>
+                  <a href={card.urlGithub}><img src={github} alt="" /></a>
+                  <a href={card.url}><img src={url} alt="" /></a>
+                </div>
+              </span>
             </div>
           );
         })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     },
   },
   plugins: [
-    
+    require('tailwindcss-animated')
   ],
 }
 
