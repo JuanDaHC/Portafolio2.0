f8d9b5da2ea65658c9d2b477d4f38e1c9947c13d
alexander muñoz <alexander12345my@gmail.com>
2023-08-20 16:21:34 -0500

    services 90%

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,30 @@ import Services from "./views/services/Services";
 import Redes from "./components/redes/Redes";
 import { useState } from "react";
 
-function App() {
-  const [lightMode, setLightMode] = useState(false);
 
- 
+function App() {
+
+  const [lightMode, setLightMode] = useState(false);
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <main className={`${lightMode ? "white" : "dark"}`}>
-      <Nav lightMode={lightMode} setLightMode={setLightMode} />
-      <Home />
-      <Projects />
-      <Redes/>
-      <Services/>
+      <Nav scrollToSection={scrollToSection} lightMode={lightMode} setLightMode={setLightMode} />
+      <div id="home" className="section">
+        <Home />
+      </div>
+      <div id="projects" className="section">
+        <Projects />
+      </div>
+      <div id="services" className="section">
+        <Services />
+      </div>
+      <Redes />
     </main>
   );
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -6,8 +6,9 @@ import moon from "../../assets/moon.svg";
 interface NavProps {
   lightMode: boolean;
   setLightMode: React.Dispatch<React.SetStateAction<boolean>>;
+  scrollToSection: React.Dispatch<React.SetStateAction<string>>;
 }
-const Nav: React.FC<NavProps> = ({lightMode, setLightMode}) => {
+const Nav: React.FC<NavProps> = ({lightMode, setLightMode, scrollToSection}) => {
 
   const light = () => {
     lightMode === false ? setLightMode(true) : setLightMode(false)
@@ -17,16 +18,16 @@ const Nav: React.FC<NavProps> = ({lightMode, setLightMode}) => {
       <nav className={styles.nav}>
         <h1 className={styles.logo}>JAMY</h1>
         <div className="flex gap-8 font-bold text-lg">
-          <a>
+          <a onClick={() => scrollToSection('home')}>
             <span>H</span>ome
           </a>
-          <a>
+          <a  onClick={() => scrollToSection('projects')}>
             <span>P</span>rojects
           </a>
-          <a>
+          <a  onClick={() => scrollToSection('services')}>
             <span>S</span>ervices
           </a>
-          <a>
+          <a onClick={() => scrollToSection('about')}>
             <span>A</span>bout
           </a>
             <img onClick={light} className="w-7" src={lightMode === false ? sun : moon} alt="sun" />
